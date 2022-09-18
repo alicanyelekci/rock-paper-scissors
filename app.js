e344bec0b767bb65029d7de9c32dc014f1f4d0d9
Ali Can Yelekci <acyelekci@gmail.com>
2022-09-18 15:49:15 +0300

    altan try

--- app.js
+++ app.js
@@ -3,15 +3,22 @@ const choices = ["rock", "paper", "scissors"];
 const playerSelection = "rock";
 const computerSelection = getComputerChoice();
 
+function game(rounds) {
+    for (let i = 1; i <= rounds; i++) {
+        playRound(playerSelection, computerSelection)
+        if ()
+    }
+}
+
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
 
     if (playerSelection == "rock" && computerSelection == "rock" || playerSelection == "paper" && computerSelection == "paper" || playerSelection == "scissors" && computerSelection == "scissors")
-        return console.log("Tie!");
+        return 0;
     else if (playerSelection == "rock" && computerSelection == "scissors" || playerSelection == "paper" && computerSelection == "rock" || playerSelection == "scissors" && computerSelection == "paper")
-        return console.log("You win!");
+        return 1;
     else if (playerSelection == "scissors" && computerSelection == "rock" || playerSelection == "rock" && computerSelection == "paper" || playerSelection == "paper" && computerSelection == "scissors")
-        return console.log("You lose!");
+        return -1;
     else return alert("Error: Invalid entry!")
 }
 
