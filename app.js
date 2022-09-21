5b5ab22665aec595cb15e33475b9ba45b45c154f
Ali Can Yelekci <acyelekci@gmail.com>
2022-09-21 13:41:01 +0300

    Simple UI added

--- app.js
+++ app.js
@@ -1,35 +1,46 @@
 const choices = ["rock", "paper", "scissors"];
+let player = 0;
+let computer = 0;
 
-function game(rounds) {
-    let player = 0;
-    let computer = 0;
+const rock = document.querySelector("#rock-button");
+const paper = document.querySelector("#paper-button");
+const scissors = document.querySelector("#scissors-button");
 
-    for (let i = 1; i <= rounds; i++) {
-        const playerSelection = prompt("Rock, Paper, Scissors?").toLowerCase();
-        const computerSelection = getComputerChoice();
-        if (playerSelection == computerSelection) continue;
-
-        playRound(playerSelection, computerSelection)
-
-        if (playRound(playerSelection, computerSelection) == 1) player++;
-        else if (playRound(playerSelection, computerSelection) == 0) computer++;
-    }
-
-    if (player > computer) console.log("You win!");
-    else if (player < computer) console.log("You lose!");
-    else console.log("Tie!");
-}
+rock.addEventListener('click', () => playRound("rock", getComputerChoice()));
+paper.addEventListener('click', () => playRound("paper", getComputerChoice()));
+scissors.addEventListener('click', () => playRound("scissors", getComputerChoice()));
 
 function playRound(playerSelection, computerSelection) {
+    if (playerSelection == "rock" && computerSelection == "scissors" || playerSelection == "paper" && computerSelection == "rock" || playerSelection == "scissors" && computerSelection == "paper") player++;
+    else if (playerSelection === computerSelection);
+    else computer++;
 
-    if (playerSelection == "rock" && computerSelection == "scissors" || playerSelection == "paper" && computerSelection == "rock" || playerSelection == "scissors" && computerSelection == "paper") return true;
-    return false;
+    updateScoreBoard(player, computer)
+
+    if (player === 5) {
+        alert(`You Win!, Player = ${player} - Computer = ${computer}`);
+        resetGame();
+    }
+    else if (computer === 5) {
+        alert(`Computer Won!, Player = ${player} - Computer = ${computer}`);
+        resetGame();
+    }
+}
+
+function resetGame() {
+    player = 0;
+    computer = 0;
+    document.getElementById("scoreboard").innerText = `Player = ${player} - Computer = ${computer}`;
+}
+
+function updateScoreBoard(player, computer) {
+    document.getElementById("scoreboard").innerText = `Player = ${player} - Computer = ${computer}`;
 }
 
 function getComputerChoice() {
-    return choices[randomInt()];
+    return choices[randomOfThree()];
 }
 
-function randomInt() {
+function randomOfThree() {
     return Math.floor(Math.random() * 3);
 }
